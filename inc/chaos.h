480ca1b9d86e163198016309f697dd4d3aeea414
Abdelilah <abdelilah.khossan@gmail.com>
2022-04-11 15:35:23 +0000

    Include necessary librairies, define constants

--- inc/chaos.h
+++ inc/chaos.h
@@ -2,16 +2,40 @@
 #define __CHAOS_H__
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <errno.h>
+#include <sys/errno.h>
 
 #include <mlx.h>
+
+#define WIN_WIDTH	700
+#define	WIN_HEIGTH	500
+#define WIN_TITLE	"CHAOS EQUATION SIMULATION"
+#define NUM_POINTS	5
+
+extern int		*POINTS[];
 
 typedef struct	s_chaos
 {
 	void	*mlx_ptr;
-	void	*mlx_win;
+	void	*win_ptr;
 	int		err;
 }				t_chaos;
+
+
+
+/*
+**	Functions definitions
+*/
+
+t_chaos	*chaos_init(void);
+
+
+/*
+**	End of functions definitions
+*/
+
+
 
 #endif
