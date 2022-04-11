d98722a42a6b80f74dc0df3e36fc64653844577e
Abdelilah <abdelilah.khossan@gmail.com>
2022-04-11 15:36:25 +0000

    Test mlx window

--- src/main.c
+++ src/main.c
@@ -1,10 +1,44 @@
-
 #include "chaos.h"
 
-int	main(int ac, char **av)
+
+void	set_coordinates()
 {
-	(void)ac, (void)av;
-	void *ptr = mlx_init();
-	free(ptr);
+	int	POINTS[NUM_POINTS][2] = {
+		{1, 2},
+		{-1, 0},
+		{4, 10},
+		{2, 5},
+		{-7, 3},
+	};
+	(void)POINTS;
+}
+
+int		main(void)
+{
+	t_chaos	*chaos;
+
+	chaos = chaos_init();
+	set_coordinates();
+	if (!chaos)
+	{
+		printf("chaos: %s\n", strerror(errno));
+		return (errno);
+	}
+	mlx_loop(chaos->mlx_ptr);
+	mlx_destroy_window(chaos->mlx_ptr, chaos->win_ptr);
 	return (0);
+}
+
+t_chaos	*chaos_init(void)
+{
+	t_chaos	*ptr;
+
+	errno = 0;
+	ptr = (t_chaos *) malloc(sizeof(t_chaos));
+	if (ptr)
+	{
+		ptr->mlx_ptr = mlx_init();
+		ptr->win_ptr = mlx_new_window(ptr->mlx_ptr, WIN_WIDTH, WIN_HEIGTH, WIN_TITLE);
+	}
+	return (ptr);	
 }
